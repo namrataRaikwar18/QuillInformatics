52393e7846af9ac3ec8d11c48e6636779874c9d6
NamrataRaikwar2002 <109650775+namrataRaikwar18@users.noreply.github.com>
2022-07-27 10:26:04 +0530

    feat: desktop navbar

---
+++ src/Components/DesktopNav/DesktopNav.jsx
@@ -0,0 +1,15 @@
+import './DesktopNav.css'
+
+const DesktopNav = () => {
+  return (
+    <div className="desktopNav displayflex">
+      <ul className="allNavLinks displayFlex ">
+        <li className="navLinks">Services</li>
+        <li className="navLinks">About Us</li>
+        <li className="navLinks">Contact Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export { DesktopNav }
